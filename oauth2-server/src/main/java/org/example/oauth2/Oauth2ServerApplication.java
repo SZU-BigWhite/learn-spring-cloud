021d5b057904ea7a80a02e3c50cbb574f495417b
Bigwhite-Mao Feng Wan <bigwhite-maofeng.wan@cn.abb.com>
2024-01-08 16:22:43 +0800

    init

--- oauth2-server/src/main/java/org/example/oauth2/Oauth2ServerApplication.java
+++ oauth2-server/src/main/java/org/example/oauth2/Oauth2ServerApplication.java
@@ -1,16 +1,11 @@
 package org.example.oauth2;
 
-import org.example.oauth2.test.Thread1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.HashMap;
 
 @SpringBootApplication
 public class Oauth2ServerApplication {
     public static void main(String[] args) {
-        Thread1 thread1=new Thread1();
-        new Thread(thread1).start();
         SpringApplication.run(Oauth2ServerApplication.class,args);
     }
 }
--- oauth2-server/src/main/java/org/example/oauth2/test/Thread1.java
+++
@@ -1,8 +0,0 @@
-package org.example.oauth2.test;
-
-public class Thread1 implements Runnable{
-    @Override
-    public void run() {
-        System.out.println(123);
-    }
-}
